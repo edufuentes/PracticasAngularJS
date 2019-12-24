d0850170ced492a08dd13d3dfe16f1771700a8f1
ER <edufuentes2323@gmail.com>
2019-12-24 12:06:43 -0300

    uso de la directiva ng-repeat para mostrar arreglos simples y complejos

--- js/app.js
+++ js/app.js
@@ -4,8 +4,27 @@
 
     app.controller('listadoCtrl', ['$scope', function($scope) {
 
+
+        $scope.listado = ["Fernando Herrera", "Melissa Flores", "Juan Carlos Pineda", "Maria Perez"];
+
+        $scope.listadoProfesores = {
+            profesores: [{
+                    nombre: "Fernando Herrera",
+                    edad: 29,
+                    clase: "PEE"
+                }, {
+                    nombre: "Melissa Flores",
+                    edad: 24,
+                    clase: "ICE"
+                },
+                {
+                    nombre: "Juan Carlos",
+                    edad: 42,
+                    clase: "MAT110"
+                }
+            ]
         }
 
-    ]);
+    }]);
 
 })();

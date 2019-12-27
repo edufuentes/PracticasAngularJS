4c2a48fd7342bc9a392af4491ae24da8b75b3f0d
ER <edufuentes2323@gmail.com>
2019-12-27 16:11:57 -0300

    se cargo el json con el http
    si usas chrome te dara un error de CORS pero en es por el navegador en mozilla se muestra el contenido del archivo JSON la visual del navegador

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
-(function(){
+(function() {
 
-var app = angular.module('ejemplosApp',[ ]);
-
-
-
-app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
-  
-  
-
-
-  
+    var app = angular.module('ejemplosApp', []);
 
 
 
-}]);
+    app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+
+        $scope.profesores = {};
+
+
+        $http.get('json/profesores.json').success(function(data) {
+            //codigo cuando es correcta la ejucion de la peticion
+
+            $scope.profesores = data.profesores;
+
+
+
+
+        });
+
+
+
+    }]);
 
 
 
